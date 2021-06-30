e6312d35ce2d63fee0cc0f19647b25ddf682df3c
Ayomide Bakare <bakkareayomideo@gmail.com>
2021-06-30 14:26:46 +0100

    #1 try 0pdd again

--- src/states/voter.py
+++ src/states/voter.py
@@ -3,6 +3,11 @@ from abc import ABC
 from .base import BaseState
 from ..messages.vote import RequestVoteMessage, CastVoteMessage
 
+"""
+@todo #1:30min Let's move this state decorator out of the states folder.
+It is not a state but a shared capability of follower and candidate states
+"""
+
 
 class Voter(BaseState, ABC):
 
@@ -10,10 +15,11 @@ class Voter(BaseState, ABC):
         BaseState.__init__(self)
         self._last_vote = None
 
+    # @todo #1 This is a test of 0pdd, to be removed.
     def handle_vote_request(self, message: RequestVoteMessage):
         """This is called when there is a vote request."""
-        if(self._last_vote is None and
-           message.data["lastLogIndex"] >= self._server._lastLogIndex):
+        if (self._last_vote is None and
+                message.data["lastLogIndex"] >= self._server._lastLogIndex):
             self._last_vote = message.sender
             return self._cast_vote_message(message, vote_for=True)
         else:
